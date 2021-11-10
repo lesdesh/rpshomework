901de376687a4d8ed5905672b4033f7945a9eda3
Leslie DeShaw <l_deshaw@fanshaweonline.ca>
2021-11-09 23:21:07 -0500

    addedHealthBar

--- main.py
+++ main.py
@@ -6,39 +6,55 @@ while gameVars.player is False:
     gameVars.player = input("Choose your weapon: rock, paper or scissors: ")
     gameVars.computer = gameVars.choices[randint(0, 2)]
 
+    heart_symbol = u'\u2764'
+
     print("player chose: " + gameVars.player)
     print("computer chose: " + gameVars.computer)
 
     if gameVars.computer == gameVars.player:
         # tie - nothing else to compare, so it'll exit
+        print("==========")
         print("tie! try again")
+        print("==========")
 
     elif gameVars.player == "rock":
         if gameVars.computer == "paper":
+            print("==========")
             print("you lose!")
-            gameVars.playerLives = gameVars.playerLives - 1
+            print("==========")
+            gameVars.playerLives = gameVars.playerLives - 1 
         else:
+            print("==========")
             print("you win!")
+            print("==========")
             gameVars.computerLives = gameVars.computerLives - 1
 
     elif gameVars.player == "paper":
         if gameVars.computer == "scissors":
+            print("==========")
             print("you lose!")
+            print("==========")
             gameVars.playerLives = gameVars.playerLives - 1
         else:
+            print("==========")
             print("you win!")
+            print("==========")
             gameVars.computerLives = gameVars.computerLives - 1
 
     elif gameVars.player == "scissors":
         if gameVars.computer == "rock":
+            print("==========")
             print("you lose!")
+            print("==========")
             gameVars.playerLives = gameVars.playerLives - 1
         else:
+            print("==========")
             print("you win!")
+            print("==========")
             gameVars.computerLives = gameVars.computerLives - 1
 
-    print("player life count: " + str(gameVars.playerLives))
-    print("computer life count: " + str(gameVars.computerLives))
+    print("player life count: " + heart_symbol * gameVars.playerLives + str(gameVars.playerLives))
+    print("computer life count: " + heart_symbol * gameVars.computerLives + str(gameVars.computerLives))
 
     if gameVars.playerLives == 0:
         winLose.winorlose("lost")
